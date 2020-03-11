fba0d8ae33e3dbfe27a0130a829a8ff5da78b248
David Ross <daboross@daboross.net>
2020-03-11 00:27:50 -0700

    Add gem starter code

---
+++ lib/tildeconfig.rb
@@ -0,0 +1,6 @@
+require "tildeconfig/version"
+
+module Tildeconfig
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/tildeconfig/version.rb
@@ -0,0 +1,3 @@
+module Tildeconfig
+  VERSION = "0.1.0"
+end
