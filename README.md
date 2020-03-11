66507a525f82cde22151237bc1439f2ea7ec5d0a
David Ross <daboross@daboross.net>
2020-03-11 01:15:22 -0700

    Add more rspec tests

--- README.md
+++ README.md
@@ -4,3 +4,18 @@ DSL for managing user space configuration settings
 [HW5.3 Document](https://docs.google.com/document/d/1MDWj7eHZowquE_PKR8kccA7BAukDoNmrVssVaJJluIc/edit?usp=sharing)
 
 [Proposal Document](https://docs.google.com/document/d/1EHTfP45b6x5dCoXL99iaWSdz9eBOa10NrwevSIOJLJM/edit?usp=sharing)
+
+## Testing
+
+To install testing packages locally, use
+
+```
+bundle install --path vendor/bundle
+# (path isn't too important)
+```
+
+then run tests using
+
+```
+bundle exec rspec spec
+```
--- spec/module_spec.rb
+++ spec/module_spec.rb
@@ -18,4 +18,30 @@ describe Tildeconfig::TildeMod do
         x.file "source"
         x.file "source2" "destination2"
     end
+    it "can define custom methods" do
+        # surely a smarter way to do this?
+        dummy = double('dummy')
+        # this expectation just ensures our new function is called exactly once
+        expect(dummy).to receive(:method1)
+
+        Tildeconfig::TildeMod.def_cmd :my_method do |m, arg1, arg2|
+            expect(arg1).to eq(1)
+            expect(arg2).to eq(42)
+            dummy.method1
+        end
+
+        m = Tildeconfig::TildeMod.new
+        m.my_method 1, 42
+    end
+    it "Custom methods can use further methods" do
+        Tildeconfig::TildeMod.def_cmd :my_method do |m|
+            m.install do
+                print "no-op"
+            end
+        end
+
+        m = Tildeconfig::TildeMod.new
+        expect(m).to receive(:install)
+        m.my_method
+    end
 end
