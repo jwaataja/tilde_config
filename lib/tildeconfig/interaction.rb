4116ade236145baeed2a57a663c914654832ad97
Jason Waataja <jwaataja@cs.washington.edu>
2021-04-21 14:32:40 -0700

    Add new ask method

--- lib/tildeconfig/interaction.rb
+++ lib/tildeconfig/interaction.rb
@@ -1,4 +1,4 @@
-module TildeMod
+module TildeConfig
   # Methods for interacting with the user.
   module Interaction
     class << self
@@ -26,6 +26,63 @@ module TildeMod
 
           puts "Please answer 'y' or 'n'."
         end
+      end
+
+      def options_prompt(prompt, options, default_response)
+        result = prompt
+        result << ' '
+        prefixes = {}
+        option_strings = []
+        options.each do |option|
+          prefix = shortest_unique_prefix(option, options)
+          prefixes[prefix] = option
+          option_string = if !default_response.nil? &&
+                             option == default_response
+                            "[#{prefix.capitalize}]"
+                          else
+
+                            "[#{prefix}]"
+                          end
+          option_string << option[prefix.size..]
+          option_strings << option_string
+        end
+        result << option_strings.join(',')
+        result << ': '
+        [result, prefixes]
+      end
+
+      def ask_with_options(prompt, options, default_response = nil)
+        prompt, prefixes = options_prompt(prompt, options, default_response)
+        loop do
+          print prompt
+          res = $stdin.gets.chomp
+          return default_response if !default.response.nil? && res.strip.empty?
+
+          prefixes.each do |prefix, option|
+            return option if res.start_with?(prefix)
+          end
+
+          puts 'Please enter a valid response'
+        end
+      end
+
+      private
+
+      # Finds the shortest prefix of option such that only option has it as a
+      # prefix in options. If no such prefix exists, i.e. if option is a prefix
+      # of some other option in options, then returns option itself.
+      # @param option [String] an option string
+      # @param optinos [Array<String>] array of options containing +option+
+      # @return [String] the shortest prefix of option such that option is the
+      #   unique element options that has the prefix, or +options+ if no such
+      #   prefix exists
+      def shortest_unique_prefix(option, options)
+        (1..option.size).each do |i|
+          prefix = option[0...i]
+          has_prefix = options.select { |opt| opt.start_with?(prefix) }
+          return prefix if has_prefix.size == 1
+        end
+        option
       end
     end
   end
