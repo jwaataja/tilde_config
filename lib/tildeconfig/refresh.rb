33a0d67f5f3a2a8f5c37047f0e848f3dd8ff716f
Jason Waataja <jwaataja@cs.washington.edu>
2021-04-20 22:35:30 -0700

    Restructure refresh code; documentation

--- lib/tildeconfig/refresh.rb
+++ lib/tildeconfig/refresh.rb
@@ -1,5 +1,9 @@
 module TildeConfig
-  # Methods for refreshing modules.
+  # Methods for refreshing modules. Refreshing a module means to check for
+  # differences between the installed versions of files and the versions in the
+  # local repository. For files that differ, it updates the local version to be
+  # the same as the installed version. This is the opposite of the +update+
+  # command.
   module Refresh
     class << self
       # Executes refresh action for this module. For each file that differs
@@ -19,27 +23,49 @@ module TildeConfig
             puts "Warning: #{src} does not exist"
             next
           end
-          unless File.file?(src)
-            puts "Warning: #{src} is not a regular file"
-            next
+          if File.file?(src)
+            refresh_file(src, dest, should_prompt)
+          else
+            puts "Warning: unknown filetype #{src}"
           end
-          unless File.exist?(dest)
-            puts "Warning: #{dest} does not exist, skipping"
-            next
-          end
-          unless File.file?(dest)
-            puts "Warning: #{dest} is not a regular file, skipping"
-            next
-          end
-          next if FileUtils.compare_file(src, dest)
+        end
+      end
 
-          proceed = !should_prompt || Interaction.ask_yes_no(
-            "Update #{src} in repository from #{dest}? [y/N] "
-          )
-          if proceed
-            puts "Copying #{dest} to #{src}"
-            FileUtils.cp(dest, src) if proceed
-          end
+      private
+
+      # Refreshes a regular file.
+      # @param src [String] path to source file
+      # @param dest [String] path to dest file
+      # @pararm should_prompt [Boolean] if true, ask the user before updating a
+      #   file in the local repository
+      def refresh_file(src, dest, should_prompt)
+        return unless check_dest_exists(dest)
+
+        unless File.file?(dest)
+          puts "Warning: #{dest} is not a regular file, skipping"
+          return
+        end
+
+        return if FileUtils.compare_file(src, dest)
+
+        proceed = !should_prompt || Interaction.ask_yes_no(
+          "Update #{src} in repository from #{dest}? [y/N] "
+        )
+        return unless proceed
+
+        puts "Copying #{dest} to #{src}"
+        FileUtils.cp(dest, src) if proceed
+      end
+
+      # Checks if +dest+ exists and prints a prompt if not.
+      # @param dest [String] file to check
+      # @return [String] true if +dest+ exists
+      def check_dest_exists(dest)
+        if File.exist?(dest)
+          true
+        else
+          puts "Warning: #{dest} does not exist, skipping"
+          false
         end
       end
     end
