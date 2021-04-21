1ecb26f9c6b5eb8da94db0db7b68d3deaf6a7356
Jason Waataja <jwaataja@cs.washington.edu>
2021-04-20 22:45:30 -0700

    Add some refresh tests

--- spec/refresh_spec.rb
+++ spec/refresh_spec.rb
@@ -19,7 +19,52 @@ module TildeConfig
         TildeConfigSpec.suppress_output do
           Refresh.refresh(m, should_prompt: false)
         end
+        expect(File.read(src_file)).to eq('contents from dest')
         expect(FileUtils.compare_file(src_file, dest_file)).to be_truthy
+      end
+    end
+
+    it "skips files that haven't been updated" do
+      Dir.mktmpdir do |dir|
+        src_dir = File.join(dir, 'src')
+        dest_dir = File.join(dir, 'dest')
+        Dir.mkdir(src_dir)
+        Dir.mkdir(dest_dir)
+        src_file = File.join(src_dir, 'testfile')
+        dest_file = File.join(dest_dir, 'testfile')
+        contents = 'the file contents'
+        File.write(src_file, contents)
+        File.write(dest_file, contents)
+        m = TildeMod.new(:test_name)
+        m.root_dir src_dir
+        m.install_dir dest_dir
+        m.file 'testfile'
+        TildeConfigSpec.suppress_output do
+          Refresh.refresh(m, should_prompt: false)
+        end
+        expect(File.read(src_file)).to eq(contents)
+      end
+    end
+
+    it 'skips symlinked files' do
+      Dir.mktmpdir do |dir|
+        src_dir = File.join(dir, 'src')
+        dest_dir = File.join(dir, 'dest')
+        Dir.mkdir(src_dir)
+        Dir.mkdir(dest_dir)
+        src_file = File.join(src_dir, 'testfile')
+        dest_file = File.join(dest_dir, 'testfile')
+        contents = 'the file contents'
+        File.write(src_file, contents)
+        FileUtils.ln_s(File.expand_path(src_file), dest_file)
+        m = TildeMod.new(:test_name)
+        m.root_dir src_dir
+        m.install_dir dest_dir
+        m.file_sym 'testfile'
+        TildeConfigSpec.suppress_output do
+          Refresh.refresh(m, should_prompt: false)
+        end
+        expect(File.read(src_file)).to eq(contents)
       end
     end
   end
